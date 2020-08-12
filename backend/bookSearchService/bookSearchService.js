b8094202c8af009f6059d9adaead22f70897ef0b
Subhash <subhaoi@gmail.com>
2020-08-12 10:35:12 +0530

    added chat box service and booksearch service

--- backend/bookSearchService/bookSearchService.js
+++ backend/bookSearchService/bookSearchService.js
@@ -1,26 +1,33 @@
 const app = require('express')();
-const bodyParser = require('body-parser');
-const superagent = require('superagent')
-const xml2js = require('xml2js');
-const fs = require('fs');
-const parser = new xml2js.Parser({ attrkey: "ATTR" });
-require('body-parser-xml')(bodyParser);
-app.use(bodyParser.xml());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+
+const superagent = require('superagent')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+           next();
+  });
 app.get('/',(req,res)=>{
     res.send("this is our main application")
 })
 
-app.get('/search',(req,res)=>{
-    // console.log(req.body)
-    res.send("Thank You")
-
+app.post('/search',(req,res)=>{
+    console.log(req.body.text)
+    var books_json = {}
+    var url = 'https://api.itbook.store/1.0/search/' + req.body.text
     superagent
-    .get('https://api.itbook.store/1.0/search/mongo')
-    .end(function(err,res){
-
-        console.log(res.body)
-
+    .get(url)
+    .end(function(err,output){
+        console.log(output.body)
+        books_json = output.body
+        return res
+        .status(200)
+        .json({ message: "Updated Succesfully", data: books_json });
     })
 })
 
--- backend/chatService/chatService.js
+++ backend/chatService/chatService.js
@@ -12,7 +12,7 @@ io.on("connection", function (client) {
     client.on("join", function(name){
     	console.log("Joined: " + name);
         clients[client.id] = name;
-        client.emit("update", "You have connected to the server.");
+        client.emit("update", "You have been connected.");
         client.broadcast.emit("update", name + " has joined the server.")
     });
 
