4dcc7c82363439592bfcb55e04229c838bf64b34
Subhash <subhaoi@gmail.com>
2020-08-20 10:58:06 +0530

    updated site

--- backend/bookSearchService/bookSearchService.js
+++ backend/bookSearchService/bookSearchService.js
@@ -2,8 +2,6 @@ const app = require('express')();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-
 const superagent = require('superagent')
 
 app.use(function(req, res, next) {
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,4 +1,8 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+const superagent = require('superagent')
 var Book = require('../models/bookModel');
 var Cart = require('../models/cartModel');
 
@@ -6,7 +10,14 @@ app.get('/books', function(req, res) {
 
 	Book.find({}, function(err, books) {
 		if(err) throw err;
+		res.send(books);
+	});
+});
 
+app.get('/sortBooks', function(req, res) {
+	console.log(req.query.sort)
+	Book.find({}, function(err, books) {
+		if(err) throw err;
 		res.send(books);
 	});
 });
@@ -43,22 +54,40 @@ app.delete('/book', function(req, res) {
 });
 
 app.post('/book', function(req, res) {
-	var bookData = req.body.bookData;
-	var book = new Book(bookData);
-	book.save(function(err, createdBookObject) {
-		if(err) {
-			res.send({
-				success: false,
-				message: "Book not added"
-			});
-		} else {
-			res.send({
-				success: true,
-				message: "Book successfully added",
-				book: createdBookObject
-			});
+	console.log(req.body.isbn)
+	var url = 'https://www.googleapis.com/books/v1/volumes?q=+isbn=' + req.body.isbn
+	superagent
+    .get(url)
+    .end(function(err,output){
+		books_json = output.body
+		try{
+			var amount = (books_json.items[0].saleInfo.listPrice.amount)
 		}
-	});
+		catch(err){
+			var amount = Math.round(Math.random() * (1000 - 300 + 1) + 300)
+		}
+		var book = new Book({title: books_json.items[0].volumeInfo.title,
+		description:books_json.items[0].volumeInfo.description,
+		author:books_json.items[0].volumeInfo.authors[0],
+		publisher:books_json.items[0].volumeInfo.publisher,
+		category:books_json.items[0].volumeInfo.categories[0],
+		price:amount,
+		cover:books_json.items[0].volumeInfo.imageLinks.thumbnail});
+		book.save(function(err, createdBookObject) {
+			if(err) {
+				res.send({
+					success: false,
+					message: "Book not added"
+				});
+			} else {
+				res.send({
+					success: true,
+					message: "Book successfully added",
+					book: createdBookObject
+				});
+			}
+		});
+    })
 });
 
 app.put('/book', function(req, res) {
